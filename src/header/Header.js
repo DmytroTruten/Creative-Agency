afb9efe196308dc0308612b3e5926180b7ada3a8
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-07 09:40:51 +0200

    add window resizing listener

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,22 +1,47 @@
 import "./Header.css";
 import logo from "../logo.svg";
+import React from "react";
 
-function Header() {
-  return (
-    <header className="header">
-      <div className="header__logo-container">
-        <img src={logo} alt="logo" />
-        <div className="header__logo-text">
-          <span>Agency</span> Creative
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = { width: 0 };
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <div className="header__logo-container">
+          <img src={logo} alt="logo" />
+          <div className="header__logo-text">
+            <span>Agency</span> Creative
+          </div>
         </div>
-      </div>
-      <div className="header__hamburger-menu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </header>
-  );
+        <div className="header__hamburger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </header>
+    );
+  }
+
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth });
+    if (this.state.width <= 576) {
+      console.log(this.state.width, "mobile");
+    } else {
+      console.log(this.state.width, "smth else");
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
 }
 
 export default Header;
