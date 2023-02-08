bfe85f2fae7d812da6f8334de9c45ec2d544739e
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-08 11:55:21 +0200

    adjust header class methods, style logo

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,18 +1,40 @@
 import "./Header.css";
-import logo from "../logo.svg";
+import logo from "../logoMobile.svg";
+import logoDesktop from "../logoDesktop.svg";
 import React, { Component } from "react";
 
 class Header extends Component {
   constructor() {
     super();
-    this.state = { screen: window.innerWidth };
+    this.state = {
+      screen: "",
+      headerLogo: logo,
+      headerLogoTextClass: "header__logo-text header__logo-text_mobile",
+    };
   }
 
   updateWidth = () => {
-    this.setState({ screen: window.innerWidth });
+    if (window.innerWidth <= 768 && this.state.screen !== "mobile") {
+      this.setState({
+        screen: "mobile",
+        headerLogo: logo,
+        headerLogoTextClass: "header__logo-text header__logo-text_mobile",
+      });
+    } else if (window.innerWidth > 768 && this.state.screen !== "desktop") {
+      this.setState({
+        screen: "desktop",
+        headerLogo: logoDesktop,
+        headerLogoTextClass: "header__logo-text header__logo-text_desktop",
+      });
+    }
   };
 
   componentDidMount() {
+    if (window.innerWidth <= 768) {
+      this.setState({ screen: "mobile" });
+    } else {
+      this.setState({ screen: "desktop" });
+    }
     window.addEventListener("resize", this.updateWidth);
   }
 
@@ -20,12 +42,12 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="header__logo-container">
-          <img src={logo} alt="logo" />
-          <div className="header__logo-text">
+          <img src={this.state.headerLogo} alt="logo" />
+          <div className={this.state.headerLogoTextClass}>
             <span>Agency</span> Creative
           </div>
         </div>
-        {this.state.screen >= 768 && (
+        {this.state.screen === "desktop" && (
           <div className="header__navbar">
             <p>Home</p>
             <p>About</p>
@@ -33,15 +55,16 @@ class Header extends Component {
             <p>Projects</p>
           </div>
         )}
-        {this.state.screen >= 768 && (
-          <button className="header__contact-button">Contact Us</button>
-        )}
-        {this.state.screen < 768 && (
+
+        {this.state.screen === "mobile" && (
           <div className="header__hamburger-menu">
             <span></span>
             <span></span>
             <span></span>
           </div>
+        )}
+        {this.state.screen === "desktop" && (
+          <button className="header__contact-button">Contact Us</button>
         )}
       </header>
     );
