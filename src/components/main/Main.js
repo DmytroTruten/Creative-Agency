b19093cda582d931a66be1b4207729679e516500
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-09 20:28:54 +0200

    add few images to main section

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,6 @@
 import "./Main.css";
+import mainImage from "../../images/main-image.svg";
+import mainReview from "../../images/main-review.svg";
 import React, { Component } from "react";
 import Button from "../button/Button";
 
@@ -31,17 +33,23 @@ class Main extends Component {
     return (
       <div className="main-container">
         <main className={`main_${this.state.screen}`}>
-          <h1 className={`main__h1_${this.state.screen}`}>
-            Make your dream business goal come true
-          </h1>
-          <h4 className={`main__h4_${this.state.screen}`}>
-            when you need us for improve your business, then come with us to
-            help your business have reach it, you just sit and feel that goal.
-          </h4>
-          <Button
-            className={`main__start-project-button_${this.state.screen}`}
-            text="Start Project"
-          />
+          <div className="main__text-container">
+            <h1 className={`main__h1_${this.state.screen}`}>
+              Make your dream business goal come true
+            </h1>
+            <h4 className={`main__h4_${this.state.screen}`}>
+              when you need us for improve your business, then come with us to
+              help your business have reach it, you just sit and feel that goal.
+            </h4>
+            <Button
+              className={`main__start-project-button_${this.state.screen}`}
+              text="Start Project"
+            />
+          </div>
+          <div className="main__image-container">
+            <img className="main__image" src={mainImage} alt="" />
+            <img className="main__review-image" src={mainReview} alt="" />
+          </div>
         </main>
       </div>
     );
