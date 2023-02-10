1c8cd2c13e64c74ab2c8ce9bf514c6830e5ca981
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-10 11:55:08 +0200

    change scss styles structure, jsx class elements

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -32,28 +32,23 @@ class Main extends Component {
 
   render() {
     return (
-      <div className="main-container">
-        <main className={`main_${this.state.screen}`}>
-          <div className="main__text-container">
-            <h1 className={`main__h1_${this.state.screen}`}>
-              Make your dream business goal come true
-            </h1>
-            <h4 className={`main__h4_${this.state.screen}`}>
-              when you need us for improve your business, then come with us to
-              help your business have reach it, you just sit and feel that goal.
-            </h4>
-            <Button
-              className={`main__start-project-button_${this.state.screen}`}
-              text="Start Project"
-            />
-          </div>
-          <div className="main__image-container">
-            <img className="main__image" src={mainImage} alt="" />
-            <img className="main__review-image" src={mainReview} alt="" />
-            <img className="main__rate-image" src={mainRate} alt="" />
-          </div>
-        </main>
-      </div>
+      <main className={`main_${this.state.screen}`}>
+        <div className="main__text-container">
+          <h1 className="main__h1">
+            Make your dream business goal come true
+          </h1>
+          <h4 className="main__h4">
+            when you need us for improve your business, then come with us to
+            help your business have reach it, you just sit and feel that goal.
+          </h4>
+          <Button className="main__start-project-button" text="Start Project" />
+        </div>
+        <div className="main__image-container">
+          <img className="main__image" src={mainImage} alt="" />
+          <img className="main__review-image" src={mainReview} alt="" />
+          <img className="main__rate-image" src={mainRate} alt="" />
+        </div>
+      </main>
     );
   }
 }
