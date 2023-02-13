1ba70071602141744c3562f294444f8625be9daa
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-13 19:38:43 +0200

    begin styling about section for desktop version

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -32,34 +32,47 @@ class About extends Component {
   render() {
     return (
       <section className={`about_${this.state.screen}`}>
-        <div className="about__text-container">
-          <div className="about__header-container">
-            <h4 className="about__header">About Us</h4>
-            <h2 className="about__subheader">Our Teammate</h2>
-          </div>
-          <div className="about__text">
-            <p>
-              We move with make a Creative Strategy for help your business goal,
-              we help to improve your income by a services we have, make your
-              content look interesting and make people look for your business.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dicta
-              dolor magni ut cum unde sunt provident.
-            </p>
-          </div>
-          <div className="about__buttons-container">
-            <Button className="about__button" text="About Us" />
-            <Button
-              className="about__play-button"
-              text="About Us"
-              play="true"
-            />
-          </div>
+        <div className="about__header-container">
+          <h4 className="about__header">About Us</h4>
+          <h2 className="about__subheader">Our Teammate</h2>
         </div>
-        <div className="about__image-container">
-          <img className="about__image" src={aboutImage} alt="" />
-          <EllipseGrid color="pink"/>
+        <div className="about__text-image-container">
+          <div className="about__text-container">
+            <div className="about__text">
+              <p>
+                We move with make a Creative Strategy for help your business
+                goal, we help to improve your income by a services we have, make
+                your content look interesting and make people look for your
+                business.
+              </p>
+              {this.state.screen === "desktop" && (
+                <p>
+                  We move with make a Creative Strategy for help your business
+                  goal, we help to improve your income by a services we have,
+                  make your content look interesting and make people look for
+                  your business.
+                </p>
+              )}
+              {this.state.screen === "mobile" && (
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                  dicta dolor magni ut cum unde sunt provident.
+                </p>
+              )}
+            </div>
+            <div className="about__buttons-container">
+              <Button className="about__button" text="About Us" />
+              <Button
+                className="about__play-button"
+                text="Our Story"
+                play="true"
+              />
+            </div>
+          </div>
+          <div className="about__image-container">
+            <img className="about__image" src={aboutImage} alt="" />
+            <EllipseGrid color="pink" />
+          </div>
         </div>
       </section>
     );
