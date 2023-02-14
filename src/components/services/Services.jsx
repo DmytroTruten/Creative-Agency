4fae0ac9f747529409d7aa5d6a746529c5abb51f
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-14 22:40:16 +0200

    fill services grid with card components/ mobile

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,16 +1,18 @@
 import "./Services.css";
 import socialMedia from "../../images/social-media.svg";
+import searchEngine from "../../images/search-engine.svg";
+import design from "../../images/design.svg";
+import ads from "../../images/ads.svg";
 import arrowRight from "../../images/arrow-right.svg";
 import React, { Component } from "react";
 
 export class Services extends Component {
-
   constructor() {
     super();
     this.state = {
       screen: "",
-    }
-  } 
+    };
+  }
 
   updateAboutState = () => {
     if (window.innerWidth <= 768 && this.state.screen !== "mobile") {
@@ -37,29 +39,74 @@ export class Services extends Component {
           <h2 className="services__subheader">Perfect and Fast Movement</h2>
         </div>
         <div className="services__text-container">
-          <p className="services__text">We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business</p>
+          <p className="services__text">
+            We move with make a Creative Strategy for help your business goal,
+            we help to improve your income by a services we have. make your
+            content look interesting and make people look for your business
+          </p>
           <div className="services__read-more-container">
             <h4 className="services__read-more">Read More</h4>
             <img className="services__arrow-right" src={arrowRight} alt="" />
           </div>
         </div>
-        <Service />
+        <div className="services-grid">
+          <Service card="media" />
+          <Service card="engine" />
+          <Service card="design" />
+          <Service card="ads" />
+        </div>
       </section>
-    )
+    );
   }
 }
 
 class Service extends Component {
   render() {
     return (
-      <div className="services-grid">
-        <div className="services-grid__services-card">
-          <div className="services-card__content">
-            <img className="services-card__content-img" src={socialMedia} alt="" />
-            <p className="services-card__content-text">Social Media Management</p>
-          </div>
+      <div className="services-grid__services-card">
+        <div className="services-card__content">
+          {this.props.card === "media" && (
+            <div>
+              <img
+                className="services-card__content-img"
+                src={socialMedia}
+                alt=""
+              />
+              <p className="services-card__content-text">
+                Social Media Management
+              </p>
+            </div>
+          )}
+          {this.props.card === "engine" && (
+            <div>
+              <img
+                className="services-card__content-img"
+                src={searchEngine}
+                alt=""
+              />
+              <p className="services-card__content-text">
+                Social Media Management
+              </p>
+            </div>
+          )}
+          {this.props.card === "design" && (
+            <div>
+              <img className="services-card__content-img" src={design} alt="" />
+              <p className="services-card__content-text">
+                Social Media Management
+              </p>
+            </div>
+          )}
+          {this.props.card === "ads" && (
+            <div>
+              <img className="services-card__content-img" src={ads} alt="" />
+              <p className="services-card__content-text">
+                Social Media Management
+              </p>
+            </div>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
