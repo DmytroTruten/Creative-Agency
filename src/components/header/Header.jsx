e46e6dabf5f1aab4672e3fdf64b7d1e3163c9fd3
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-24 11:10:43 +0200

    change header class component to functional

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,84 +2,71 @@ import "./Header.css";
 import logo from "../../logoMobile.svg";
 import Navigation from "../navigation/Navigation";
 import Button from "../button/Button";
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
-class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      screen: "",
-      sidenavVisible: false,
+function Header() {
+  const [screen, setScreenState] = useState("");
+  const [sidenavVisible, setSidenavState] = useState(false);
+
+  useEffect(() => {
+    handleHeaderState()
+    window.addEventListener("resize", handleHeaderState);
+    
+    return () => {
+      window.removeEventListener("resize", handleHeaderState);
     };
-  }
+  }, []);
 
-  toggleSidenav = () => {
-    this.setState((previousState) => ({
-      sidenavVisible: !previousState.sidenavVisible,
-    }));
-  };
-
-  handleHeaderState = () => {
-    if (window.innerWidth <= 768 && this.state.screen !== "mobile") {
-      this.setState({
-        screen: "mobile",
-      });
-    } else if (window.innerWidth > 768 && this.state.screen !== "desktop") {
-      this.setState({
-        screen: "desktop",
-      });
+  const handleHeaderState = () => {
+    if (window.innerWidth <= 768) {
+      setScreenState("mobile");
+    } else if (window.innerWidth > 768) {
+      setScreenState("desktop");
     }
-  };
-
-  componentDidMount() {
-    this.handleHeaderState();
-    window.addEventListener("resize", this.handleHeaderState);
   }
-
-  render() {
-    return (
-      <header className={`header_${this.state.screen}`}>
-        <div className="header__logo-container">
-          <img className="header__logo" src={logo} alt="" />
-          <div className="header__logo-text">
-            <span>Agency</span> Creative
-          </div>
+  
+  return (
+    <header className={`header_${screen}`}>
+      <div className="header__logo-container">
+        <img className="header__logo" src={logo} alt="" />
+        <div className="header__logo-text">
+          <span>Agency</span> Creative
         </div>
-        {this.state.screen === "mobile" && (
-          <Fragment>
-            <div
-              onClick={this.toggleSidenav}
-              className={`header__hamburger-menu${
-                this.state.sidenavVisible ? " active" : ""
-              }`}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div
-              className={`header__sidenav${
-                this.state.sidenavVisible ? "_opened" : ""
-              }`}
-            >
-              <Navigation
-                for="sidenav-mobile"
-                screen={this.state.screen}
-                toggleSidenav={this.toggleSidenav}
-              />
-              <Button className="header__contact-button" text="Contact Us" />
-            </div>
-          </Fragment>
-        )}
-        {this.state.screen === "desktop" && (
-          <Fragment>
-            <Navigation for="navbar-desktop" screen={this.state.screen} />
+      </div>
+      {screen === "mobile" && (
+        <Fragment>
+          <div
+            onClick={() => {
+              setSidenavState(!sidenavVisible);
+            }}
+            className={`header__hamburger-menu${
+              sidenavVisible ? " active" : ""
+            }`}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`header__sidenav${sidenavVisible ? "_opened" : ""}`}>
+            <Navigation
+              for="sidenav-mobile"
+              screen={screen}
+              toggleSidenav={() => {
+                setSidenavState(!sidenavVisible);
+              }}
+            />
             <Button className="header__contact-button" text="Contact Us" />
-          </Fragment>
-        )}
-      </header>
-    );
-  }
+          </div>
+        </Fragment>
+      )}
+      {screen === "desktop" && (
+        <Fragment>
+          <Navigation for="navbar-desktop" screen={screen} />
+          <Button className="header__contact-button" text="Contact Us" />
+        </Fragment>
+      )}
+    </header>
+  );
 }
 
 export default Header;
