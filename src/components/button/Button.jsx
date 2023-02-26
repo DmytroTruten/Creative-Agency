675db89fb4bf8a8cdf83a6c11638a0ea12d58b11
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-26 20:20:39 +0200

    finished changing theme mechanics

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,7 +4,7 @@ import playIcon from "../../images/about/play-circle.svg";
 function Button(props) {
   return (
     <button className={props.className}>
-      {props.play === "true" && <img src={playIcon} alt="" />}
+      {props.play === "true" && <img className="about__play-circle" src={playIcon} alt="" />}
       {props.text}
     </button>
   );
